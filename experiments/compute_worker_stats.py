1ffe467e799161993bcb4e387c4f475e0c480d29
kliegr <kliegr@gmail.com>
2018-07-19 09:44:06 +0200

    sync with arXiv:1803.01316

--- experiments/compute_worker_stats.py
+++ experiments/compute_worker_stats.py
@@ -4,19 +4,93 @@ import crowdflower
 import csv
 experiment1=[{"name":"TRAFFIC", "version":1, "statfile":'postprocessed_crowdflower_output/traffic_accidents_types.arules.pairs.with_stats.csv',"files":["crowdflower_output/V1Guidelines/f791490.csv"]},{"name":"QUALITY","version":1,"statfile":'postprocessed_crowdflower_output/mercer.pairs.with_stats.csv',"files":["crowdflower_output/V1Guidelines/f834875.csv"]},{"name":"MOVIES","version":1,"statfile":'postprocessed_crowdflower_output/movies.pairs.with_stats.csv',"files":["crowdflower_output/V1Guidelines/f836187.csv"]},{"name":"MUSHROOMS", "version":1,"statfile":'postprocessed_crowdflower_output/mushrooms.pairs.with_stats.csv',"files":["crowdflower_output/V1Guidelines/f956290.csv","crowdflower_output/V1Guidelines/f956954.csv"]},{"name":"MUSHROOMS", "version":2,"statfile":'postprocessed_crowdflower_output/mushrooms.pairs.with_stats.csv',"files":["crowdflower_output/V2guidelines_without_hard_test_questions/f855702.csv","crowdflower_output/V2guidelines_without_hard_test_questions/f860274.csv"]},{"name":"TRAFFIC", "version":2, "statfile":'postprocessed_crowdflower_output/traffic_accidents_types.arules.pairs.with_stats.csv',"files":["crowdflower_output/V2guidelines_without_hard_test_questions/f843991.csv","crowdflower_output/V2guidelines_without_hard_test_questions/f857983.csv"]},{"name":"QUALITY","version":2,"statfile":'postprocessed_crowdflower_output/mercer.pairs.with_stats.csv',"files":["crowdflower_output/V2guidelines_without_hard_test_questions/f843982.csv","crowdflower_output/V2guidelines_without_hard_test_questions/f857955.csv"]},{"name":"MOVIES","version":2,"statfile":'postprocessed_crowdflower_output/movies.pairs.with_stats.csv',"files":["crowdflower_output/V2guidelines_without_hard_test_questions/f843002.csv"]},{"name":"MOVIES", "version":3, "statfile":'postprocessed_crowdflower_output/movies.pairs.with_stats.csv',"files":["crowdflower_output/V3guidelines_with_explicit_frequency/f843916.csv"]}]
 experiment2=[{"version":"1", "file":'crowdflower_output/linda/f854177.csv'}, {"version":"2", "file":'crowdflower_output/linda/f857928.csv' },{"version":"3a", "file":'crowdflower_output/linda/f858990.csv' },{"version":"3b", "file":'crowdflower_output/linda/f860282.csv' }]
+experiment3a=[{"name":"MUSHROOM", "version":"ATTIMP", "files":["crowdflower_output/attributeimportance/f876173.csv","crowdflower_output/attributeimportance/f876832.csv"]}]
+experiment3b=[{"name":"TRAFFIC", "version":"ATTIMP", "files":["crowdflower_output/attributeimportance/f876663.csv"]}]
+experiment4a=[{"name":"MUSHROOM", "version":"LITIMP", "files":["crowdflower_output/literalimportance/f855714.csv","crowdflower_output/literalimportance/f859005.csv"]}]
+experiment4b=[{"name":"MOVIES", "version":"LITIMP", "file":"crowdflower_output/literalimportance/f842577.csv"}]
+experiment4c=[{"name":"TRAFFIC", "version":"LITIMP", "file":"crowdflower_output/literalimportance/f842634.csv"}]
+experiment4d=[{"name":"QUALITY", "version":"LITIMP", "file":"crowdflower_output/literalimportance/f842668.csv"}]
 
+dfWorkers = set()
+dfAll = pd.DataFrame(columns=["test"])
 
 df_ = pd.DataFrame(columns=["dataset","v","judg","workers","usa","gbr","can","avg dur","reasons"])
 
 for i in range(0,len(experiment1)):
     inputCSVFile = experiment1[i:i+1]
     df=crowdflower.read_crowdflower_full_export(inputCSVFile,extrafields=["_worker_id","_country","_started_at","_created_at","what_is_the_rationale_for_your_choice"])
-    durations = pd.to_datetime(df._created_at) - pd.to_datetime(df._started_at)
-    df_.loc[i] = [experiment1[i]["name"], str(experiment1[i]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)), len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+    #drop pairs containing manually input rule
+    if experiment1[i]["name"]=="MUSHROOMS":
+        # the same as "manualIncorrect"  - one rule was manually added to the discovered rule set and then excluded from subsequent analyses
+        df=df.drop(df[df.r1id=="20624"].index)
+        df=df.drop(df[df.r2id=="20624"].index)
 
+    durations = pd.to_datetime(df._created_at) - pd.to_datetime(df._started_at)
+    dfWorkers.update(df["_worker_id"].tolist())
+    df_.loc[i] = [experiment1[i]["name"], str(experiment1[i]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)), len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+    if dfAll.empty:
+        dfAll = df
+    else:
+        dfAll=pd.concat([dfAll,df])
 for i in range(0,len(experiment2)):
     df=pd.read_csv(experiment2[i]["file"], delimiter=",", quoting=csv.QUOTE_ALL,index_col=False)
+    #drop pairs containing manually input rule
     durations = pd.to_datetime(df._created_at) - pd.to_datetime(df._started_at)
-    df_.loc[i+len(experiment1)] = ["Linda", str(experiment2[i]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)),len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+    df_.loc[i+len(experiment1)] = ["Linda", str(experiment2[i]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)),len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]            
+    # workers in experiment 2 are intentionally omitted from the total count of unique workers
+#attribute importance
+##mushroom
+df1=pd.read_csv(experiment3a[0]["files"][0], delimiter=",", quoting=csv.QUOTE_ALL)    
+df2=pd.read_csv(experiment3a[0]["files"][1], delimiter=",", quoting=csv.QUOTE_ALL)    
+df=pd.concat([df1,df2])
+
+durations = pd.to_datetime(df._created_at) - pd.to_datetime(df._started_at)
+df_.loc[len(df_)+1] = [experiment3a[0]["name"], str(experiment3a[0]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)), len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+df=df[df["what_is_the_rationale_for_your_choice"].str.len()>10]
+df_.loc[len(df_)+1] = [experiment3a[0]["name"] + "_onlyused", str(experiment3a[0]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)), len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+dfWorkers.update(df["_worker_id"].tolist())
+df_.to_csv("stats/workers.csv")
+dfAll=pd.concat([dfAll,df])
+##traffic
+df=pd.read_csv(experiment3b[0]["files"][0], delimiter=",", quoting=csv.QUOTE_ALL)    
+
+durations = pd.to_datetime(df._created_at) - pd.to_datetime(df._started_at)
+df_.loc[len(df_)+1] = [experiment3b[0]["name"], str(experiment3b[0]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)), len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+df=df[df["what_is_the_rationale_for_your_choice"].str.len()>10]
+df_.loc[len(df_)+1] = [experiment3b[0]["name"] + "_onlyused", str(experiment3b[0]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)), len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+dfWorkers.update(df["_worker_id"].tolist())
+df_.to_csv("stats/workers.csv")
+dfAll=pd.concat([dfAll,df])
+    #literal importance"
+## mushroom
+df1=pd.read_csv(experiment4a[0]["files"][0], delimiter=",", quoting=csv.QUOTE_ALL)    
+df2=pd.read_csv(experiment4a[0]["files"][1], delimiter=",", quoting=csv.QUOTE_ALL)    
+df=pd.concat([df1,df2])
+
+durations = pd.to_datetime(df._created_at) - pd.to_datetime(df._started_at)
+df_.loc[len(df_)+1] = [experiment4a[0]["name"], str(experiment4a[0]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)), len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+dfAll=pd.concat([dfAll,df])
+dfWorkers.update(df["_worker_id"].tolist())
+## movies
+df=pd.read_csv(experiment4b[0]["file"], delimiter=",", quoting=csv.QUOTE_ALL)    
+durations = pd.to_datetime(df._created_at) - pd.to_datetime(df._started_at)
+df_.loc[len(df_)+1] = [experiment4b[0]["name"], str(experiment4b[0]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)), len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+dfAll=pd.concat([dfAll,df])
+dfWorkers.update(df["_worker_id"].tolist())
+## traffic
+df=pd.read_csv(experiment4c[0]["file"], delimiter=",", quoting=csv.QUOTE_ALL)    
+durations = pd.to_datetime(df._created_at) - pd.to_datetime(df._started_at)
+df_.loc[len(df_)+1] = [experiment4c[0]["name"], str(experiment4c[0]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)), len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+dfAll=pd.concat([dfAll,df])
+dfWorkers.update(df["_worker_id"].tolist())
+## quality
+df=pd.read_csv(experiment4d[0]["file"], delimiter=",", quoting=csv.QUOTE_ALL)    
+durations = pd.to_datetime(df._created_at) - pd.to_datetime(df._started_at)
+df_.loc[len(df_)+1] = [experiment4d[0]["name"], str(experiment4d[0]["version"]),str(len(df)),str(len(df._worker_id.unique())),str(len(df[df._country=="USA"])),str(len(df[df._country=="GBR"])), str(len(df[df._country=="CAN"])), str(durations.mean(axis=0)), len(df[df["what_is_the_rationale_for_your_choice"].str.len()>10])]
+dfAll=pd.concat([dfAll,df])
+dfWorkers.update(df["_worker_id"].tolist())
 
 df_.to_csv("stats/workers.csv")
+dfAll.to_csv("stats/all-df-contatenated.csv")
+print(dfWorkers)
+print("unique workers:" + str(len(dfWorkers)))
--- experiments/crowdflower.py
+++ experiments/crowdflower.py
@@ -4,7 +4,7 @@ import hashlib
 
 def stringToInt(text):
   #return int(hashlib.md5(text).hexdigest(), 16)
-  return str(int(hashlib.md5(text).hexdigest(), 16))[0:5]
+  return str(int(hashlib.md5(text.encode('utf-8')).hexdigest(), 16))[0:5]
 
 def fixIDs(df):
  #the following three lines are needed only for TA
@@ -19,26 +19,26 @@ def fixIDs(df):
  df.r2id=df.r2id.str[0:5]
  return df
 
-def read_crowdflower_full_export(datasets):
+def read_crowdflower_full_export(datasets,extrafields=[]):
   df_all=pd.DataFrame()
   for dataset in datasets:
     df_stats=pd.read_csv(dataset["statfile"], delimiter=",", quoting=csv.QUOTE_ALL,dtype={"r1id":"str","r2id":"str"})    
-    print dataset["statfile"]
-    print dataset["name"]
+    print(dataset["statfile"])
+    print(dataset["name"])
     df_full_all=pd.DataFrame()
     for file in dataset["files"]:
-      print file
-      df_full=pd.read_csv(file, delimiter=",", quoting=csv.QUOTE_ALL,index_col=False,dtype={"r1id":"str","r2id":"str"},usecols=["r1id","r2id","r1","r2","which_of_the_rules_do_you_find_as_more_plausible"])
+      print (file)
+      df_full=pd.read_csv(file, delimiter=",", quoting=csv.QUOTE_ALL,index_col=False,dtype={"r1id":"str","r2id":"str"},usecols=["r1id","r2id","r1","r2","which_of_the_rules_do_you_find_as_more_plausible"] + extrafields)
       df_full_all=pd.concat([df_full_all,df_full],ignore_index=True)
     df_full_all=fixIDs(df_full_all)
     orig_size=len(df_full_all)
     df_full_all=df_full_all.merge(df_stats, on=["r1id","r2id"],how="inner")
     if len(df_full_all)!=orig_size:
       if dataset["name"] == "MUSHROOMS" and len(df_full_all)==300:
-        print "Intentionaly omitting "+ str(orig_size-len(df_full_all)) + " rows for MUSHROOM dataset"
+        print ("Intentionaly omitting "+ str(orig_size-len(df_full_all)) + " rows for MUSHROOM dataset")
       else:
-        print "Something wrong after joining statistics. Original rows: " + str(orig_size) + " New rows:" + str(len(df_full_all))
-    print len(df_full_all)
+        print ("Something wrong after joining statistics. Original rows: " + str(orig_size) + " New rows:" + str(len(df_full_all)))
+    print(len(df_full_all))
     df_full_all["dataset"]=dataset["name"]
     df_all=pd.concat([df_all,df_full_all])
   return df_all
@@ -62,7 +62,7 @@ def get_table_with_derived_metrics(df):
   metrics=[("r1len","r2len","lenDelta","lenRatio"),("r1DepthMax","r2DepthMax","DepthMaxDelta","DepthMaxRatio"),("r2DepthMin","r1DepthMin","DepthMinDelta","DepthMinRatio"),("r1LabelLengthSum","r2LabelLengthSum","LabelLengthSumDelta","LabelLengthSumRatio"),("r1LabelLengthMax","r2LabelLengthMax","LabelLengthMaxDelta","LabelLengthMaxRatio"),("r1LabelLengthAvg","r2LabelLengthAvg","LabelLengthAvgDelta","LabelLengthAvgRatio"),("r2LabelLengthMin","r1LabelLengthMin","LabelLengthMinDelta","LabelLengthMinRatio"),("r1minDistance","r2minDistance","MinDistanceDelta","MinDistanceRatio"),("r1maxDistance","r1maxDistance","MaxDistanceDelta","MaxDistanceRatio"),("r21mindepthLCS","r1mindepthLCS","MinDepthLCSDelta","MinDepthLCSRatio"),("r1PageRankMax","r2PageRankMax","PageRankMaxDelta","PageRankMaxRatio"),("r2PageRankMin","r1PageRankMin","PageRankMinDelta","PageRankMinRatio"),("r1PageRankAvg","r2PageRankAvg","PageRankAvgDelta","PageRankAvgRatio"),("r1conf","r2conf","ConfDelta","ConfRatio"),("r1supp","r2supp","SuppDelta","SuppRatio"),("r1LitImpMax","r2LitImpMax","LitImpMaxDelta","LitImpMaxRatio"),("r2LitImpMin","r1LitImpMin","LitImpMinDelta","LitImpMinRatio"),("r1LitImpAvg","r2LitImpAvg","LitImpAvgDelta","LitImpAvgRatio"),("r1AttImpMax","r2AttImpMax","AttImpMaxDelta","AttImpMaxRatio"),("r2AttImpMin","r1AttImpMin","AttImpMinDelta","AttImpMinRatio"),("r1AttImpAvg","r2AttImpAvg","AttImpAvgDelta","AttImpAvgRatio"),("r1AttImpSum","r2AttImpSum","AttImpSumDelta","AttImpSumRatio"),("r1LitImpSum","r2LitImpSum","LitImpSumDelta","LitImpSumRatio")]  
   for (r1_metric,r2_metric, delta_name,ratio_name) in metrics:
     if not (r1_metric in df.columns):
-      print "omitting " + r1_metric + " not found"
+      print ("omitting " + r1_metric + " not found")
       continue
     df_export[r1_metric]=df[r1_metric]
     df_export[r2_metric]=df[r2_metric]
